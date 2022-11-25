761588b2094c9acf34b43cd4a451a9f10e2f1449
SayemAbdullah1 <dxsayem0@gmail.com>
2022-11-25 16:25:32 +0600

    initial setup of verse reseller

---
+++ src/Pages/Shared/DisplayError/DisplayError.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DisplayError = () => {
+    return (
+        <div>
+            <h2>Display eror</h2>
+        </div>
+    );
+};
+
+export default DisplayError;
