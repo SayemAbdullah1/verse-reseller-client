b020ccd96a78e0b0a724892e52da4551ba126468
SayemAbdullah1 <dxsayem0@gmail.com>
2023-02-13 22:02:24 +0600

    category cahnges

--- src/Pages/Home/Category/CategoryCart.js
+++ src/Pages/Home/Category/CategoryCart.js
@@ -6,10 +6,13 @@ const CategoryCart = ({ singleCat }) => {
     console.log(singleCat);
     return (
         <div className='rounded-xl relative'>
-            <div className='absolute text-white bg-black/40 w-full h-full rounded-xl'>
-                <p className='font-bold text-2xl px-2 pt-4 mb-6'>{name}</p>
+            <div data-aos="fade-up"
+                data-aos-duration="3000" className='absolute text-white bg-black/40 w-full h-full rounded-xl'>
+                <p className='font-bold text-3xl px-2 pt-4 mb-6' data-aos="fade-up"
+                    data-aos-duration="3000">{name}</p>
 
-                <Link to={`/category/${category_id}`}>
+                <Link to={`/category/${category_id}`} data-aos="fade-up"
+                    data-aos-duration="3000">
 
                 <button className='btn btn-secondary border-white mt-3 text-black mx-2 bottom-4'>See All Products</button>
                 </Link>
